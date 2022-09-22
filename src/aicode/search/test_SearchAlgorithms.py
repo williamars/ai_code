bdc865e5200931bd4373f0f4fe89caf48829a125
slimkaki <rafaalmada98@gmail.com>
2022-09-22 09:51:12 -0300

    iniciando teste das buscas

---
+++ src/aicode/search/test_SearchAlgorithms.py
@@ -0,0 +1,8 @@
+from SearchAlgorithms import BuscaLargura
+# from SearchAlgorithms import BuscaProfundidade
+# from SearchAlgorithms import BuscaProfundidadeIterativa
+# from Graph import Node
+
+def test_BuscaLargura():
+    s = BuscaLargura()
+    s.search(Node())
