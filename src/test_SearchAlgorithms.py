5431c620ec1640033593f8a19446e7e1bfbd5af9
Bruno Rodrigues <bf17669@gmail.com>
2022-09-22 11:31:13 -0300

    De result!=None para custo esperado

--- src/test_SearchAlgorithms.py
+++ src/test_SearchAlgorithms.py
@@ -2,22 +2,25 @@ from aicode.search.SearchAlgorithms import *
 from U2 import U2
 
 def test_BuscaLargura():
-    print('Busca de Custo Uniforme')
+    print('\nBusca em Largura')
     state = U2(False, False, False, False, False, ' ')
     algorithm = BuscaLargura()
     result = algorithm.search(state)
-    assert result != None
+    print("custo",result.g)
+    assert result.g >= 17
 
 def test_BuscaProfundidade():
-    print('Busca de Custo Uniforme')
+    print('\nBusca em Profundidade')
     state = U2(False, False, False, False, False, ' ')
     algorithm = BuscaProfundidade()
     result = algorithm.search(state, 20)
-    assert result != None
+    print("custo",result.g)
+    assert result.g > 17
 
 def test_BuscaCustoUniforme():
-    print('Busca de Custo Uniforme')
+    print('\nBusca de Custo Uniforme')
     state = U2(False, False, False, False, False, ' ')
     algorithm = BuscaCustoUniforme()
     result = algorithm.search(state)
-    assert result != None
+    print("custo",result.g)
+    assert result.g <= 17
