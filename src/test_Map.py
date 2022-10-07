113b01743f358f38b6ae1c3e8691c25234d1a14f
Guilherme Teixeira Batista <38350062+guidiamond@users.noreply.github.com>
2022-10-07 13:36:12 -0300

    Testes integracao map (#15)
    * Add testes unitarios para o map
    
    * Delete teste (arquivo nao utilizado)
    
    * Add it tests para o map
    
    Co-authored-by: Fabrício J. Barth <fabricio.barth@gmail.com>

--- src/test_Map.py
+++ src/test_Map.py
@@ -5,8 +5,49 @@ from aicode.search.SearchAlgorithms import AEstrela
 import time 
 from Map import *
 
-Map.createHeuristics()
 Map.createArea()
+Map.createHeuristics()
+
+def test_is_goal_retorna_corretamente_0():
+    print('\n#### Test metodo is_goal retorna corretamente 0 ####')
+    state = Map('i', 0, 'i', 'x')
+    assert state.is_goal() == False
+
+def test_is_goal_retorna_corretamente_1():
+    print('\n#### Test metodo is_goal retorna corretamente 1 ####')
+    state = Map('i', 0, 'i', 'i')
+    assert state.is_goal() == True
+
+def test_successors_retorna_corretamente():
+    print('\n#### Test metodo successors retorna sucessores corretamente ####')
+    state = Map('i', 0, 'i', 'x').sucessors()
+    expected_values = [
+        {'city': 'e', 'cost_value': 2, 'operator': 'e', 'goal': 'x'},
+        {'city': 'h', 'cost_value': 2, 'operator': 'h', 'goal': 'x'}
+    ]
+    for i in range(len(expected_values)):
+        assert expected_values[i] == state[i].__dict__
+
+def test_algoritmo_encontra_solucao_existente():
+    print('\n#### Teste algoritmo encontra solucao que existe ####')
+    state = Map('i', 0, 'i', 'x')
+    algorithm = AEstrela()
+    res = algorithm.search(state)
+    assert res != None
+
+def test_algoritmo_encontra_melhor_solucao_possivel_a_o():
+    print('\n#### Teste algoritmo encontra solucao de a para o ####')
+    state = Map('a', 0, 'a', 'o')
+    algorithm = AEstrela()
+    res = algorithm.search(state)
+    assert res.g == 8
+
+def test_algoritmo_encontra_melhor_solucao_possivel_n_x():
+    print('\n#### Teste algoritmo encontra solucao de n para x ####')
+    state = Map('n', 0, 'n', 'x')
+    algorithm = AEstrela()
+    res = algorithm.search(state)
+    assert res.g == 3
 
 def test_a_x():
     state = Map('a',0, 'a','x')
@@ -24,7 +65,6 @@ def test_a_x():
     assert result.g == 12
     print(result.show_path())
     
-
 def test_menor_caminho_n_ate_x():
     state = Map('n', 0, 'n', 'x')
     algorithm = AEstrela()
@@ -57,7 +97,6 @@ def test_b_x():
     assert result.g == 9
     print(result.show_path())
 
-
 def test_menor_caminho_l_ate_o():
     state = Map('l', 0, 'l', 'o')
     algorithm = AEstrela()
@@ -73,7 +112,6 @@ def test_menor_caminho_l_ate_o():
     print('')
     assert result.g == 11
     print(result.show_path())
-
 
 def test_c_o():
     state = Map('c',0, 'c','o')
@@ -105,3 +143,4 @@ def test_menor_caminho_x_ate_o():
     print('')
     assert result.g == 16
     print(result.show_path())
+
