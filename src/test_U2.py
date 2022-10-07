636d9653aae90df38f940e12fc8fc2150c3c1c2f
gubenites <gubenites99@gmail.com>
2022-10-07 13:27:47 -0300

    Atualizado (#20)
    Co-authored-by: gustavomfb <gustavo.benites@onepartners.com.br>
    Co-authored-by: Fabrício J. Barth <fabricio.barth@gmail.com>

--- src/test_U2.py
+++ src/test_U2.py
@@ -1,6 +1,3 @@
-from re import T
-import re
-# from turtle import st
 from aicode.search.SearchAlgorithms import BuscaCustoUniforme
 from U2 import U2
 
@@ -11,12 +8,48 @@ def test_menor_caminho():
     assert result.g <= 17
     print(result.show_path())
 
+def test_larry():
+    '''
+        Este teste leva em consideração que o integrante mais lento da banda é o unico que se
+        encontra do lado da ponte em que esta a lampada, tornando então o tempo de resolução do
+        problema maior do que o tempo em que os mesmos tem para chegar ao show.
+    '''
+
+    state = U2(False, False, False, False, True, True, ' ')
+    algorithm = BuscaCustoUniforme()
+    result = algorithm.search(state)
+
+    assert result.g > 17
+
+def test_lado_direito():
+    '''
+        Este teste faz a consideração que todos os membros da banda se encontram do lado direito
+        da ponte, enquanto a lanterna esta do lado esquerdo, tornando então a solução impossivel. 
+    '''
+
+    state = U2(True, True, True, True, True, False, ' ')
+    algorithm = BuscaCustoUniforme()
+    result = algorithm.search(state)
+
+    assert result == None
+    
+def test_lado_esquerdo():
+    '''
+        Este teste faz a consideração que todos os membros da banda se encontram do lado esquerdo
+        da ponte, enquanto a lanterna esta do lado direito, tornando então a solução impossivel. 
+    '''
+
+    state = U2(False, False, False, False, False, True, ' ')
+    algorithm = BuscaCustoUniforme()
+    result = algorithm.search(state)
+
+    assert result == None
+
 def test_laterna_sozinha():
     state = U2(False, False, False, False, True, ' ')
     algorith = BuscaCustoUniforme()
     result = algorith.search(state)
     assert result == None
-    
 
 def test_todos_no_final():
     state = U2(True, True, True, True, False, ' ')
@@ -30,5 +63,4 @@ def test_mais_rapido_caminho():
     result = algorithm.search(state)
     assert result.g == 1
     print(result.show_path())
-
-test_todos_no_final()
+    
